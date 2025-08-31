a40ed4750b028d0231f41a9b80d7da68b84abbc8
Peter Verzijl <peter@alteregogames.com>
2025-08-31 12:50:46 +0200

    Added new cards; added pdfs; added export for pdf file. Added fonts. Added animations to cards.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Notes:
+
+## Exporting the PDF
+ - Install ImageMagick 
+ - Install ghost script https://ghostscript.com/releases/gsdnld.html
+ - Run the export.bat file.
---
+++ assets/export.bat
@@ -0,0 +1,3 @@
+magick -density 300 "GeoMystics SYMBOLICS A6_CS6 1-11 - unfiltered cards only.pdf" page-%03d-1x.jpg
+magick -density 150 "GeoMystics SYMBOLICS A6_CS6 1-11 - unfiltered cards only.pdf" page-%03d-0.5x.jpg
+magick -density 75 "GeoMystics SYMBOLICS A6_CS6 1-11 - unfiltered cards only.pdf" page-%03d-0.25x.jpg
